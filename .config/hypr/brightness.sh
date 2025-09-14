222cf06e247e8bc1e065512ed8f5929efd4ee20c
Jimmy Cerra <15009093+Frodojj@users.noreply.github.com>
2025-09-13 22:56:37 -0400

    Got hyprland/idle/lock working to my satisfaction
    brightness.sh uses ddcutil. Modify with brightnessctl if you use that instead

---
+++ .config/hypr/brightness.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# Copyright 2025 Jimmy Cerra 
+# MIT license: Permission granted to use without restriction as long as the
+# copyright and license are included with all whole or substantial copies.
+# PROVIDED "AS IS" WITH NO WARRANTY. THE AUTHOR(S) ARE NOT LIABLE FOR DAMAGES.
+
+
+save_brightness() {
+	ddcutil -t getvcp 10 | awk '{print $4}' > ~/.brightness
+}
+
+set_brightness() {
+	ddcutil setvcp 10 "$1"
+}
+
+restore_brightness() {
+	if test -f ~/.brightness; then
+		ddcutil setvcp 10 $(cat ~/.brightness)
+	fi
+}
+
+case "$1" in
+	save)
+		save_brightness
+		if [ "$#" -eq 2 ]; then
+			set_brightness "$2"
+		fi
+		;;
+	set)
+		set_brightness "$2"
+		;;
+	restore)
+		restore_brightness
+		;;
+	*)
+		echo "params: save [optional new int value] | set [new int value] | restore"
+		;;
+esac
