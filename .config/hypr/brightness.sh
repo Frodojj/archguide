66643cc5bda6cd504b55cb7a74e9d7aa6b4c72c5
Jimmy Cerra <15009093+Frodojj@users.noreply.github.com>
2025-09-20 19:23:25 -0400

    script calls hyprctl & ddcutil now

--- .config/hypr/brightness.sh
+++ .config/hypr/brightness.sh
@@ -4,19 +4,22 @@
 # copyright and license are included with all whole or substantial copies.
 # PROVIDED "AS IS" WITH NO WARRANTY. THE AUTHOR(S) ARE NOT LIABLE FOR DAMAGES.
 
+get_brightness() {
+	ddcutil -t getvcp 10 | awk '{print $4}'
+}
 
 save_brightness() {
 	# Store variable in hyprctl process
-	hyprctl keyword env BRIGHTNESS,$(ddcutil -t getvcp 10 | awk '{print $4}')
+	hyprctl keyword env SAVED_BRIGHTNESS,$(get_brightness)
 }
 
 set_brightness() {
-	hyprctl dispatch exec ddcutil setvcp 10 "$1"
+	ddcutil setvcp 10 "$1"
 }
 
 restore_brightness() {
 	# Variable needs interpreted by hyprctl process rather than this process
-	hyprctl dispatch exec ddcutil setvcp 10 \$BRIGHTNESS
+	hyprctl dispatch exec ddcutil setvcp 10 \$SAVED_BRIGHTNESS
 }
 
 case "$1" in
@@ -29,10 +32,13 @@ case "$1" in
 	set)
 		set_brightness "$2"
 		;;
+	get)
+		get_brightness
+		;;
 	restore)
 		restore_brightness
 		;;
 	*)
-		echo "params: save [optional new int value] | set [new int value] | restore"
+		echo "params: get | set [new int value] | save [optional new int value] | restore"
 		;;
 esac
